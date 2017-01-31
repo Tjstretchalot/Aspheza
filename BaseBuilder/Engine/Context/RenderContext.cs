2d4e1cccf529f1590327c033a03a75769a10e27f
Timothy Moore <mtimothy983@gmail.com>
2017-01-31 10:53:50 -0800

    Add SpriteTile
    RenderContext:
      - Add Content
    
    SpriteTile:
      - First commit

--- BaseBuilder/Engine/Context/RenderContext.cs
+++ BaseBuilder/Engine/Context/RenderContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,7 @@ namespace BaseBuilder.Engine.Context
     /// </summary>
     public struct RenderContext
     {
+        public ContentManager Content;
         public GraphicsDeviceManager Graphics;
         public SpriteBatch SpriteBatch;
     }
---
+++ BaseBuilder/Engine/World/Tiles/SpriteTile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseBuilder.Engine.Context;
+using BaseBuilder.Engine.Math2D.Double;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace BaseBuilder.Engine.World.Tiles
+{
+    public class SpriteTile : Tile
+    {
+        protected string SpriteName;
+
+        public SpriteTile(string spriteName)
+        {
+            SpriteName = spriteName;
+        }
+
+        public override void Render(RenderContext context, PointD2D screenTopLeft)
+        {
+            var texture = context.Content.Load<Texture2D>(SpriteName);
+
+            context.SpriteBatch.Draw(texture,
+                new Rectangle(
+                    (int)(screenTopLeft.X + CollisionMesh.Left), (int)(screenTopLeft.Y + CollisionMesh.Top), 
+                    (int)(CollisionMesh.Width), (int)(CollisionMesh.Height)
+                    ), Color.White);
+        }
+    }
+}
