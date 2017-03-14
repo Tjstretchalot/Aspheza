087683fd7b0a487d8bef9db6af4f44158f512f2f
Timothy Moore <mtimothy983@gmail.com>
2017-03-14 11:40:14 -0700

    Fix errors

---
+++ BaseBuilder/Screens/GameScreens/TaskOverlays/TaskItems/ComplexTaskItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseBuilder.Engine.Context;
+using BaseBuilder.Engine.State;
+using BaseBuilder.Engine.World.Entities.EntityTasks;
+
+namespace BaseBuilder.Screens.GameScreens.TaskOverlays.TaskItems
+{
+    /// <summary>
+    /// A complex task item is set up to allow for 
+    /// </summary>
+    public class ComplexTaskItem : SimpleTaskItem
+    {
+        public override IEntityTask CreateEntityTask(ITaskable taskable, SharedGameState sharedState, LocalGameState localState, NetContext netContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsValid(SharedGameState sharedState, LocalGameState localState, NetContext netContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
